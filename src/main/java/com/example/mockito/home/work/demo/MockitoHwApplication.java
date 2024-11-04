c41b104c1351d600f9b9cef5f830de24b8721e99
Stupendos <topakovdenis671@gmail.com>
2024-11-04 20:04:09 +0700

    ASD

---
+++ src/main/java/com/example/mockito/home/work/demo/MockitoHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockito.home.work.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoHwApplication.class, args);
+	}
+
+}
